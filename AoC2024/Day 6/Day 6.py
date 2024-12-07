297d5db9b2fa1bf3979c54dd9971c18e8e58fb53
kgirtz <kubeman88@gmail.com>
2024-12-07 00:44:08 -0500

    Day 6 partial

--- AoC2024/Day 6/Day 6.py
+++ AoC2024/Day 6/Day 6.py
@@ -1,21 +1,90 @@
 import pathlib
 import sys
 import os
+from collections import defaultdict
+
+from point import Point
+from space import Space
+
+
+turn_right: dict[str, str] = {'^': '>',
+                              '>': 'v',
+                              'v': '<',
+                              '<': '^'}
 
 
 def parse(puzzle_input: str):
     """Parse input"""
-    return [line for line in puzzle_input.split('\n')]
+    return puzzle_input.split('\n')
+
+
+def get_next(pos: Point, direction: str) -> Point:
+    match direction:
+        case '^':
+            return pos.above()
+        case '>':
+            return pos.right()
+        case 'v':
+            return pos.below()
+        case '<':
+            return pos.left()
+        case _:
+            raise ValueError('invalid direction')
+
+
+def path(s: Space) -> set[Point]:
+    visited: set[Point] = set()
+    cur_dir: str = '^'
+    cur_pos: Point = s.initial_position(cur_dir)
+    while s.valid_point(cur_pos):
+        visited.add(cur_pos)
+        next_pos: Point = get_next(cur_pos, cur_dir)
+        if next_pos in s.items['#']:
+            cur_dir = turn_right[cur_dir]
+            cur_pos = get_next(cur_pos, cur_dir)
+        else:
+            cur_pos = next_pos
+    return visited
+
+
+def loops(s: Space) -> bool:
+    visited: dict[Point, set[str]] = defaultdict(set)
+    cur_dir: str = '^'
+    cur_pos: Point = s.initial_position(cur_dir)
+    while s.valid_point(cur_pos):
+        visited[cur_pos].add(cur_dir)
+        next_pos: Point = get_next(cur_pos, cur_dir)
+        if next_pos in s.items['#']:
+            cur_dir = turn_right[cur_dir]
+            cur_pos = get_next(cur_pos, cur_dir)
+        else:
+            cur_pos = next_pos
+
+        if cur_dir in visited[cur_pos]:
+            return True
+
+    return False
 
 
 def part1(data):
     """Solve part 1"""
-    return data
+    s: Space = Space(data, '^#')
+
+    return len(path(s))
 
 
 def part2(data):
     """Solve part 2"""
-    return data
+    s: Space = Space(data, '^#')
+
+    num_loops: int = 0
+    possible: set[Point] = path(s) - {s.initial_position('^')}
+    for pt in possible:
+        s.items['#'].add(pt)
+        if loops(s):
+            num_loops += 1
+        s.items['#'].remove(pt)
+    return num_loops
 
 
 def solve(puzzle_input: str):
@@ -31,8 +100,8 @@ def solve(puzzle_input: str):
 if __name__ == '__main__':
     DIR: str = f'{os.path.dirname(sys.argv[0])}/'
 
-    PART1_TEST_ANSWER = None
-    PART2_TEST_ANSWER = None
+    PART1_TEST_ANSWER = 41
+    PART2_TEST_ANSWER = 6
 
     file: pathlib.Path = pathlib.Path(DIR + 'part1_test.txt')
     if file.exists() and PART1_TEST_ANSWER is not None:
