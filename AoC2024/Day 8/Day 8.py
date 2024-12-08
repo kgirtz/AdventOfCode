af7b493deee1bc46fffdf37ff7ae4820bd954903
kgirtz <kubeman88@gmail.com>
2024-12-08 15:52:13 -0500

    Day 8 initial

--- AoC2024/Day 8/Day 8.py
+++ AoC2024/Day 8/Day 8.py
@@ -2,20 +2,90 @@ import pathlib
 import sys
 import os
 
+from space import Space
+from point import Point
+
+
+class AntennaMap(Space):
+    def __init__(self, input_map) -> None:
+        super().__init__(input_map)
+
+        self.locations: dict[str, set[Point]] = self.items
+
+    def frequencies(self) -> set[str]:
+        return set(self.locations.keys())
+
+    def calculate_antinodes(self, a: Point, b: Point) -> set[Point]:
+        if a == b:
+            return set()
+
+        # a to b
+        rise: int = 2 * (b.y - a.y)
+        run: int = 2 * (b.x - a.x)
+        node_ab: Point = Point(a.x + run, a.y + rise)
+
+        # b to a
+        rise = 2 * (a.y - b.y)
+        run = 2 * (a.x - b.x)
+        node_ba: Point = Point(b.x + run, b.y + rise)
+
+        return {node for node in (node_ab, node_ba) if self.valid_point(node)}
+
+    def calculate_harmonic_antinodes(self, a: Point, b: Point) -> set[Point]:
+        if a == b:
+            return set()
+
+        antinodes: set[Point] = set()
+
+        # a to b
+        rise: int = b.y - a.y
+        run: int = b.x - a.x
+        cur_node: Point = a
+
+        while self.valid_point(cur_node):
+            antinodes.add(cur_node)
+            cur_node = Point(cur_node.x + run, cur_node.y + rise)
+
+        cur_node = a
+        while self.valid_point(cur_node):
+            antinodes.add(cur_node)
+            cur_node = Point(cur_node.x - run, cur_node.y - rise)
+
+        return antinodes
+
+    def find_antinodes(self, freq: str, *, harmonics: bool = False) -> set[Point]:
+        antinodes: set[Point] = set()
+        antennas: list[Point] = list(self.locations[freq])
+        for i, a in enumerate(antennas[:-1]):
+            for b in antennas[i + 1:]:
+                if harmonics:
+                    antinodes.update(self.calculate_harmonic_antinodes(a, b))
+                else:
+                    antinodes.update(self.calculate_antinodes(a, b))
+        return antinodes
+
 
 def parse(puzzle_input: str):
     """Parse input"""
-    return [line for line in puzzle_input.split('\n')]
+    return puzzle_input.split('\n')
 
 
 def part1(data):
     """Solve part 1"""
-    return data
+    antennas: AntennaMap = AntennaMap(data)
+    antinodes: set[Point] = set()
+    for f in antennas.frequencies():
+        antinodes.update(antennas.find_antinodes(f))
+    return len(antinodes)
 
 
 def part2(data):
     """Solve part 2"""
-    return data
+    antennas: AntennaMap = AntennaMap(data)
+    antinodes: set[Point] = set()
+    for f in antennas.frequencies():
+        antinodes.update(antennas.find_antinodes(f, harmonics=True))
+    return len(antinodes)
 
 
 def solve(puzzle_input: str):
@@ -31,8 +101,8 @@ def solve(puzzle_input: str):
 if __name__ == '__main__':
     DIR: str = f'{os.path.dirname(sys.argv[0])}/'
 
-    PART1_TEST_ANSWER = None
-    PART2_TEST_ANSWER = None
+    PART1_TEST_ANSWER = 14
+    PART2_TEST_ANSWER = 34
 
     file: pathlib.Path = pathlib.Path(DIR + 'part1_test.txt')
     if file.exists() and PART1_TEST_ANSWER is not None:
