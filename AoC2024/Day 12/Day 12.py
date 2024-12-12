dd358487ae592492082220634959eed59ba42585
kgirtz <kubeman88@gmail.com>
2024-12-12 09:33:09 -0500

    Day 12 part 1

--- AoC2024/Day 12/Day 12.py
+++ AoC2024/Day 12/Day 12.py
@@ -1,21 +1,73 @@
 import pathlib
 import sys
 import os
+from typing import Collection, Generator
+
+from point import Point
+from space import Space
+
+
+class Garden(Space):
+    @staticmethod
+    def area(region: Collection[Point]) -> int:
+        return len(region)
+
+    @staticmethod
+    def perimeter(region: Collection[Point]) -> int:
+        perim: int = 0
+        for pt in region:
+            perim += 4 - len(pt.neighbors().intersection(region))
+        return perim
+
+    @staticmethod
+    def sides(region: Collection[Point]) -> int:
+        sides: int = 0
+
+        return sides
+
+    def price(self, region: Collection[Point]) -> int:
+        return self.perimeter(region) * self.area(region)
+
+    def bulk_price(self, region: Collection[Point]) -> int:
+        return self.sides(region) * self.area(region)
+
+    def expand(self, start: Point) -> set[Point]:
+        plant: str = self[start]
+        pts: set[Point] = set()
+        new_pts: set[Point] = {start}
+        while new_pts:
+            pts |= new_pts
+            new_new_pts: set[Point] = set()
+            for pt in new_pts:
+                new_new_pts |= self.neighbors(pt) & self.items[plant]
+            new_pts = new_new_pts - pts
+        return pts
+
+    def regions(self) -> Generator[set[Point], None, None]:
+        for plots in self.items.values():
+            plots = set(plots)
+            while plots:
+                seed: Point = plots.pop()
+                region: set[Point] = self.expand(seed)
+                plots -= region
+                yield region
 
 
 def parse(puzzle_input: str):
     """Parse input"""
-    return [line for line in puzzle_input.split('\n')]
+    return puzzle_input
 
 
 def part1(data):
     """Solve part 1"""
-    return data
+    garden: Garden = Garden(data)
+    return sum(garden.price(r) for r in garden.regions())
 
 
 def part2(data):
     """Solve part 2"""
-    return data
+    garden: Garden = Garden(data)
+    return sum(garden.bulk_price(r) for r in garden.regions())
 
 
 def solve(puzzle_input: str):
@@ -31,8 +83,8 @@ def solve(puzzle_input: str):
 if __name__ == '__main__':
     DIR: str = f'{os.path.dirname(sys.argv[0])}/'
 
-    PART1_TEST_ANSWER = None
-    PART2_TEST_ANSWER = None
+    PART1_TEST_ANSWER = 772
+    PART2_TEST_ANSWER = 436
 
     file: pathlib.Path = pathlib.Path(DIR + 'part1_test.txt')
     if file.exists() and PART1_TEST_ANSWER is not None:
