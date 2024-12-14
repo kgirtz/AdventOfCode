e0b909bdc9cbf5c24bc9b1b53b6cf1df77774f87
kgirtz <kubeman88@gmail.com>
2024-12-13 20:58:40 -0500

    Day 13 initial

--- AoC2024/Day 13/Day 13.py
+++ AoC2024/Day 13/Day 13.py
@@ -1,21 +1,55 @@
 import pathlib
 import sys
 import os
+import re
+
+from point import Point, ORIGIN
 
 
 def parse(puzzle_input: str):
     """Parse input"""
-    return [line for line in puzzle_input.split('\n')]
+    claw_machines: list[tuple[Point, Point, Point]] = []
+    for m in puzzle_input.split('\n\n'):
+        numbers: list[int] = [int(n) for n in re.findall(r'\d+', m)]
+        a: Point = Point(*numbers[0:2])
+        b: Point = Point(*numbers[2:4])
+        prize: Point = Point(*numbers[4:6])
+        claw_machines.append((a, b, prize))
+    return claw_machines
+
+
+def min_cost(a: Point, b: Point, prize: Point, start: Point = ORIGIN) -> int:
+    b_numerator: int = a.x * prize.y - a.y * prize.x
+    b_denominator: int = a.x * b.y - a.y * b.x
+    if b_numerator % b_denominator:
+        return -1
+
+    b_presses: int = b_numerator // b_denominator
+    if (prize.x - b.x * b_presses) % a.x:
+        return -1
+
+    a_presses: int = (prize.x - b.x * b_presses) // a.x
+    return 3 * a_presses + b_presses
 
 
 def part1(data):
     """Solve part 1"""
-    return data
+    total_cost: int = 0
+    for a, b, prize in data:
+        cost: int = min_cost(a, b, prize)
+        if cost != -1:
+            total_cost += cost
+    return total_cost
 
 
 def part2(data):
     """Solve part 2"""
-    return data
+    total_cost: int = 0
+    for a, b, prize in data:
+        cost: int = min_cost(a, b, Point(prize.x + 10000000000000, prize.y + 10000000000000))
+        if cost != -1:
+            total_cost += cost
+    return total_cost
 
 
 def solve(puzzle_input: str):
