13332b337b03803c544d93bbbd22047dcb63b516
kgirtz <kubeman88@gmail.com>
2024-12-14 01:15:45 -0500

    Day 14 initial

--- AoC2024/Day 14/Day 14.py
+++ AoC2024/Day 14/Day 14.py
@@ -1,21 +1,89 @@
 import pathlib
 import sys
 import os
+from typing import Iterable
+
+from point import Point, PointTuple
+
+
+class Robot:
+    def __init__(self, pos: PointTuple, vel: PointTuple) -> None:
+        self.position: Point = Point(*pos)
+        self.velocity: Point = Point(*vel)
+
+    def __repr__(self) -> str:
+        return f'{self.__class__.__name__}({self.position})'
+
+    def move(self, seconds: int, width: int, height: int) -> None:
+        self.position = self.velocity * seconds + self.position
+        self.position = Point(self.position.x % width, self.position.y % height)
+
+    def quadrant(self, width: int, height: int) -> int:
+        middle_x: int = width // 2
+        middle_y: int = height // 2
+        if self.position.x > middle_x and self.position.y < middle_y:
+            return 1
+        if self.position.x < middle_x and self.position.y < middle_y:
+            return 2
+        if self.position.x < middle_x and self.position.y > middle_y:
+            return 3
+        if self.position.x > middle_x and self.position.y > middle_y:
+            return 4
+
+
+def safety_factor(robots: Iterable[Robot], width: int, height: int) -> int:
+    factor: int = 1
+    for quadrant in range(1, 5):
+        factor *= len([r for r in robots if r.quadrant(width, height) == quadrant])
+    return factor
+
+
+def print_robots(robots: Iterable[Robot], width: int, height: int) -> None:
+    robot_positions: set[Point] = {r.position for r in robots}
+    lines: list[str] = []
+    for y in range(height):
+        line: str = ''
+        for x in range(width):
+            if (x, y) in robot_positions:
+                line += '#'
+            else:
+                line += ' '
+        lines.append(line)
+    print('\n'.join(lines))
+
+
+def touching(robots: Iterable[Robot]) -> int:
+    robot_positions: set[Point] = {r.position for r in robots}
+    return sum(len(r.position.neighbors(include_corners=True) & robot_positions) for r in robots)
 
 
 def parse(puzzle_input: str):
     """Parse input"""
-    return [line for line in puzzle_input.split('\n')]
+    robots: list[Robot] = []
+    for line in puzzle_input.split('\n'):
+        p_str, v_str = line.split()
+        p: PointTuple = tuple(int(n) for n in p_str.removeprefix('p=').split(','))
+        v: PointTuple = tuple(int(n) for n in v_str.removeprefix('v=').split(','))
+        robots.append(Robot(p, v))
+    return robots
 
 
 def part1(data):
     """Solve part 1"""
-    return data
+    space_width: int = 101  # test = 11, input = 101
+    space_height: int = 103  # test = 7, input = 103
+    for robot in data:
+        robot.move(100, space_width, space_height)
+    return safety_factor(data, space_width, space_height)
 
 
 def part2(data):
-    """Solve part 2"""
-    return data
+    """Solve part 2"""  # 6752
+    space_width: int = 101
+    space_height: int = 103
+    for robot in data:
+        robot.move(6752, space_width, space_height)
+    print_robots(data, space_width, space_height)
 
 
 def solve(puzzle_input: str):
@@ -31,7 +99,7 @@ def solve(puzzle_input: str):
 if __name__ == '__main__':
     DIR: str = f'{os.path.dirname(sys.argv[0])}/'
 
-    PART1_TEST_ANSWER = None
+    PART1_TEST_ANSWER = None  # 12  # change size of space
     PART2_TEST_ANSWER = None
 
     file: pathlib.Path = pathlib.Path(DIR + 'part1_test.txt')
