a3c30a2b8d8eac400f6c19475bf6e83f75194a14
kgirtz <kubeman88@gmail.com>
2024-12-16 15:36:56 -0500

    Day 15 clean up

--- AoC2024/Day 15/Day 15.py
+++ AoC2024/Day 15/Day 15.py
@@ -2,12 +2,11 @@ import pathlib
 import sys
 import os
 
-from xypair import XYpair, accept_tuple
+from xypair import XYpair
 from pointwalker import PointWalker, Heading
 from space import Space
 
 
-@accept_tuple
 def gps_coordinate(pt: XYpair) -> int:
     return pt.x + 100 * pt.y
 
@@ -46,17 +45,15 @@ class Warehouse(Space):
         else:
             self.boxes: set[XYpair] = self.items['O']
 
-    def move_object(self, cur_pos: XYpair, direction: str) -> None:
-        heading: Heading = {'^': Heading.NORTH, '>': Heading.EAST, 'v': Heading.SOUTH, '<': Heading.WEST}[direction]
+    def move_object(self, cur_pos: XYpair, heading: Heading) -> None:
         new_pos: XYpair = PointWalker(cur_pos, heading).next()
-
         if new_pos in self.walls:
             return
         if new_pos in self.boxes:
             if self.wide:
-                self.move_wide_box(new_pos, direction)
+                self.move_wide_box(new_pos, heading)
             else:
-                self.move_object(new_pos, direction)
+                self.move_object(new_pos, heading)
             if new_pos in self.boxes:
                 return
 
@@ -66,12 +63,11 @@ class Warehouse(Space):
             self.boxes.remove(cur_pos)
             self.boxes.add(new_pos)
 
-    def wide_box_can_move(self, cur_pos: XYpair, direction: str) -> bool:
+    def wide_box_can_move(self, cur_pos: XYpair, heading: Heading) -> bool:
         # Default box position is left
         if cur_pos in self.right_boxes:
             cur_pos = cur_pos.left()
 
-        heading: Heading = {'^': Heading.NORTH, '>': Heading.EAST, 'v': Heading.SOUTH, '<': Heading.WEST}[direction]
         if heading == Heading.WEST:
             cur_pos = cur_pos.right()
 
@@ -85,18 +81,16 @@ class Warehouse(Space):
         if new_pos not in self.boxes and (heading.horizontal() or new_pos.right() not in self.boxes):
             return True
         if heading.horizontal() or (heading.vertical() and new_pos in self.left_boxes):
-            return self.wide_box_can_move(new_pos, direction)
+            return self.wide_box_can_move(new_pos, heading)
 
         # Vertical with misaligned box(es) in the way
         blocking: set[XYpair] = self.boxes & {new_pos, new_pos.right()}
-        return all(self.wide_box_can_move(box, direction) for box in blocking)
+        return all(self.wide_box_can_move(box, heading) for box in blocking)
 
-    def move_wide_box(self, cur_pos: XYpair, direction: str) -> None:
+    def move_wide_box(self, cur_pos: XYpair, heading: Heading) -> None:
         # Default box position is left
         if cur_pos in self.right_boxes:
             cur_pos = cur_pos.left()
-
-        heading: Heading = {'^': Heading.NORTH, '>': Heading.EAST, 'v': Heading.SOUTH, '<': Heading.WEST}[direction]
         if heading == Heading.WEST:
             cur_pos = cur_pos.right()
 
@@ -107,15 +101,15 @@ class Warehouse(Space):
 
         if new_pos in self.walls or (heading.vertical() and new_pos.right() in self.walls):
             return
-        if new_pos in self.boxes and not self.wide_box_can_move(new_pos, direction):
+        if new_pos in self.boxes and not self.wide_box_can_move(new_pos, heading):
             return
-        if heading.vertical() and new_pos.right() in self.left_boxes and not self.wide_box_can_move(new_pos.right(), direction):
+        if heading.vertical() and new_pos.right() in self.left_boxes and not self.wide_box_can_move(new_pos.right(), heading):
             return
 
         if new_pos in self.boxes:
-            self.move_wide_box(new_pos, direction)
+            self.move_wide_box(new_pos, heading)
         if heading.vertical() and new_pos.right() in self.left_boxes:
-            self.move_wide_box(new_pos.right(), direction)
+            self.move_wide_box(new_pos.right(), heading)
 
         match heading:
             case Heading.EAST:
@@ -136,75 +130,31 @@ class Warehouse(Space):
 
         self.boxes = self.left_boxes | self.right_boxes
 
-    def move_robot(self, direction: str) -> None:
-        self.move_object(self.robot, direction)
-
-    def move_robot_wide(self, direction: str) -> None:
-        match direction:
-            case '>':
-                if self.robot.right() in self.walls:
-                    return
-                if self.robot.right() not in self.boxes:
-                    self.robot = self.robot.right()
-                    return
-
-                not_box: XYpair = self.robot.right(2)
-                while not_box in self.boxes:
-                    not_box = not_box.right()
-                if not_box in self.walls:
-                    return
-
-                self.robot = self.robot.right()
-                self.boxes.remove(self.robot)
-                self.boxes.add(not_box)
-                self.left_boxes -= {(x, self.robot.y) for x in range(self.robot.x, not_box.x, 2)}
-                self.left_boxes |= {(x, self.robot.y) for x in range(self.robot.x + 1, not_box.x, 2)}
-                self.right_boxes -= {(x, self.robot.y) for x in range(self.robot.x + 1, not_box.x, 2)}
-                self.right_boxes |= {(x, self.robot.y) for x in range(self.robot.x + 2, not_box.x + 1, 2)}
-
-            case '<':
-                if self.robot.left() in self.walls:
-                    return
-                if self.robot.left() not in self.boxes:
-                    self.robot = self.robot.left()
-                    return
-
-                not_box: XYpair = self.robot.left(2)
-                while not_box in self.boxes:
-                    not_box = not_box.left()
-                if not_box in self.walls:
-                    return
-
-                self.robot = self.robot.left()
-                self.boxes.remove(self.robot)
-                self.boxes.add(not_box)
-                self.right_boxes -= {(x, self.robot.y) for x in range(self.robot.x, not_box.x, -2)}
-                self.right_boxes |= {(x, self.robot.y) for x in range(self.robot.x - 1, not_box.x, -2)}
-                self.left_boxes -= {(x, self.robot.y) for x in range(self.robot.x - 1, not_box.x, -2)}
-                self.left_boxes |= {(x, self.robot.y) for x in range(self.robot.x - 2, not_box.x - 1, -2)}
+    def move_robot(self, heading: Heading) -> None:
+        self.move_object(self.robot, heading)
 
 
 def parse(puzzle_input: str):
     """Parse input"""
     warehouse_str, movement_str = puzzle_input.split('\n\n')
-    movement_str = movement_str.replace('\n', '')
-    return warehouse_str, movement_str
+    directions: list[Heading] = [Heading.from_arrow(arrow) for arrow in movement_str.replace('\n', '')]
+    return warehouse_str, directions
 
 
 def part1(data):
     """Solve part 1"""
-    warehouse_str, movement_str = data
+    warehouse_str, directions = data
     warehouse: Warehouse = Warehouse(warehouse_str)
-    for d in movement_str:
+    for d in directions:
         warehouse.move_robot(d)
     return sum(gps_coordinate(pt) for pt in warehouse.boxes)
 
 
 def part2(data):
     """Solve part 2"""
-    warehouse_str, movement_str = data
+    warehouse_str, directions = data
     warehouse: Warehouse = Warehouse(warehouse_str, wide=True)
-    for d in movement_str:
+    for d in directions:
         warehouse.move_robot(d)
     return sum(gps_coordinate(pt) for pt in warehouse.left_boxes)
 
