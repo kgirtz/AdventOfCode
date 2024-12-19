c3caf44e234055048c77a2ea2b0ae224960590ac
kgirtz <kubeman88@gmail.com>
2024-12-19 13:08:10 -0500

    Day 19 initial/clean up

--- AoC2024/Day 19/Day 19.py
+++ AoC2024/Day 19/Day 19.py
@@ -1,21 +1,40 @@
 import pathlib
 import sys
 import os
+import functools
 
 
 def parse(puzzle_input: str):
     """Parse input"""
-    return [line for line in puzzle_input.split('\n')]
+    towels_str, patterns_str = puzzle_input.split('\n\n')
+    towels: tuple[str, ...] = tuple(towels_str.split(', '))
+    patterns: list[str] = patterns_str.split('\n')
+    return towels, patterns
+
+
+@functools.cache
+def possible(pattern: str, towels: tuple[str, ...]) -> int:
+    num_matches: int = 0
+    if pattern in set(towels):
+        num_matches += 1
+    for i in range(1, len(pattern)):
+        if pattern[:i] in towels:
+            num_matches += possible(pattern[i:], towels)
+    return num_matches
 
 
 def part1(data):
     """Solve part 1"""
-    return data
+    towels, patterns = data
+    towels = tuple(sorted(towels, key=len, reverse=True))
+    return sum(bool(possible(pattern, towels)) for pattern in patterns)
 
 
 def part2(data):
     """Solve part 2"""
-    return data
+    towels, patterns = data
+    towels = tuple(sorted(towels, key=len, reverse=True))
+    return sum(possible(pattern, towels) for pattern in patterns)
 
 
 def solve(puzzle_input: str):
@@ -31,8 +50,8 @@ def solve(puzzle_input: str):
 if __name__ == '__main__':
     DIR: str = f'{os.path.dirname(sys.argv[0])}/'
 
-    PART1_TEST_ANSWER = None
-    PART2_TEST_ANSWER = None
+    PART1_TEST_ANSWER = 6
+    PART2_TEST_ANSWER = 16
 
     file: pathlib.Path = pathlib.Path(DIR + 'part1_test.txt')
     if file.exists() and PART1_TEST_ANSWER is not None:
