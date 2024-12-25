26b75bc8998447c447643ca677743a71948bb2e9
kgirtz <kubeman88@gmail.com>
2024-12-25 12:51:14 -0500

    Day 23 initial

--- AoC2024/Day 23/Day 23.py
+++ AoC2024/Day 23/Day 23.py
@@ -18,29 +18,82 @@ def network_graph(connections: Iterable[Iterable[str]]) -> dict[str, set[str]]:
     return graph
 
 
-def interconnected_trios(graph: dict[str, set[str]]) -> set[tuple[str, str, str]]:
-    trios: set[tuple[str, str, str]] = set()
+def fully_connected_trios(graph: dict[str, set[str]]) -> set[frozenset[str]]:
+    trios: set[frozenset[str]] = set()
     for node, connections in graph.items():
-        connections = list(connections)
+        connections = tuple(connections)
         for i, first in enumerate(connections[:-1]):
             for second in connections[i + 1:]:
                 if second in graph[first]:
-                    trios.add(tuple(sorted((node, first, second))))  # noqa
+                    trios.add(frozenset((node, first, second)))
     return trios
+
+
+def fully_connected_groups(graph: dict[str, set[str]]) -> set[frozenset[str]]:
+    groups: set[frozenset[str]] = fully_connected_trios(graph)
+    new_groups: set[frozenset[str]] = set()
+    changed: bool = True
+    while changed and len(groups) > 1:
+        changed = False
+        group_list: list[frozenset[str]] = list(groups)
+        print(f'{len(groups)} groups to check...')
+        for i, group1 in enumerate(group_list[:-1]):
+            if i % 1000 == 0:
+                print(i)
+            for group2 in group_list[i + 1:]:
+                if is_fully_connected(set(group1 ^ group2), graph):
+                    changed = True
+                    new_groups.add(group1 | group2)
+        groups = new_groups
+        new_groups = set()
+    return groups
+
+
+def largest_fully_connected_group(graph: dict[str, set[str]]) -> set[str]:
+    groups: set[frozenset[str]] = fully_connected_trios(graph)
+    largest_group: set[str] = set()
+    for trio in groups:
+        expanded: set[str] = fully_connected_group(trio, graph)
+        if len(expanded) > len(largest_group):
+            largest_group = expanded
+    return largest_group
+
+
+def fully_connected_group(trio: frozenset[str], graph: dict[str, set[str]]) -> set[str]:
+    group: set[str] = set(trio)
+    node: str = tuple(trio)[0]
+    possible_members: set[str] = graph[node] - group
+    while possible_members:
+        member: str = possible_members.pop()
+        if group.issubset(graph[member]):
+            group.add(member)
+    return group
+
+
+def is_fully_connected(group: set[str], graph: dict[str, set[str]]) -> bool:
+    if len(group) <= 1:
+        return True
+    member: str = group.pop()
+    if group.issubset(graph[member]):
+        return is_fully_connected(group, graph)
+    return False
+
+
+def password(computers: Iterable[str]) -> str:
+    return ','.join(sorted(computers))
 
 
 def part1(data):
     """Solve part 1"""
-    total: int = 0
-    for trio in interconnected_trios(network_graph(data)):
-        if any(name.startswith('t') for name in trio):
-            total += 1
-    return total
+    graph: dict[str, set[str]] = network_graph(data)
+    return sum(any(name.startswith('t') for name in trio) for trio in fully_connected_trios(graph))
 
 
 def part2(data):
     """Solve part 2"""
-    return data
+    graph: dict[str, set[str]] = network_graph(data)
+    #return password(max(fully_connected_groups(graph), key=len))
+    return password(largest_fully_connected_group(graph))
 
 
 def solve(puzzle_input: str):
