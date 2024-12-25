990353fd3b5038b3ba7e489fb8c199db33e2f97b
kgirtz <kubeman88@gmail.com>
2024-12-25 12:00:49 -0500

    Day 25 initial/clean up

--- AoC2024/Day 25/Day 25.py
+++ AoC2024/Day 25/Day 25.py
@@ -1,56 +1,57 @@
 import pathlib
 import sys
 import os
+from typing import Sequence
 
 
 def parse(puzzle_input: str):
     """Parse input"""
-    return [line for line in puzzle_input.split('\n')]
+    locks: list[list[int]] = []
+    keys: list[list[int]] = []
+    for grid in puzzle_input.split('\n\n'):
+        rows: list[str] = grid.split('\n')
+        top_row: str = rows[0]
+        width: int = len(top_row)
+        heights: list[int] = [[line[i] for line in rows].count('#') - 1 for i in range(width)]
+        if set(top_row) == {'#'}:
+            locks.append(heights)
+        else:
+            keys.append(heights)
+    return locks, keys
+
+
+def fits(key: Sequence[int], lock: Sequence[int]) -> bool:
+    return all(k + l <= 5 for k, l in zip(key, lock, strict=True))
 
 
 def part1(data):
     """Solve part 1"""
-    return data
-
-
-def part2(data):
-    """Solve part 2"""
-    return data
+    locks, keys = data
+    return sum(fits(key, lock) for lock in locks for key in keys)
 
 
 def solve(puzzle_input: str):
     """Solve the puzzle for the given input"""
     data = parse(puzzle_input)
     solution1 = part1(data)
-    data = parse(puzzle_input)
-    solution2 = part2(data)
-
-    return solution1, solution2
+    return solution1,
 
 
 if __name__ == '__main__':
     DIR: str = f'{os.path.dirname(sys.argv[0])}/'
 
-    PART1_TEST_ANSWER = None
-    PART2_TEST_ANSWER = None
+    PART1_TEST_ANSWER = 3
 
     file: pathlib.Path = pathlib.Path(DIR + 'part1_test.txt')
     if file.exists() and PART1_TEST_ANSWER is not None:
         puzzle_input: str = file.read_text().strip()
         assert part1(parse(puzzle_input)) == PART1_TEST_ANSWER
 
-    file = pathlib.Path(DIR + 'part2_test.txt')
-    if file.exists() and PART2_TEST_ANSWER is not None:
-        puzzle_input = file.read_text().strip()
-        assert part2(parse(puzzle_input)) == PART2_TEST_ANSWER
-
     file = pathlib.Path(DIR + 'test.txt')
     if file.exists():
         puzzle_input = file.read_text().strip()
         if PART1_TEST_ANSWER is not None:
             assert part1(parse(puzzle_input)) == PART1_TEST_ANSWER
-        if PART2_TEST_ANSWER is not None:
-            assert part2(parse(puzzle_input)) == PART2_TEST_ANSWER
 
     for infile in ('input.txt',):
         print(f'{infile}:')
