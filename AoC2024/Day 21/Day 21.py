91162689a2512969a3b8af15c262170a2fca17b3
kgirtz <kubeman88@gmail.com>
2024-12-27 00:15:23 -0500

    Day 21 part 1

--- AoC2024/Day 21/Day 21.py
+++ AoC2024/Day 21/Day 21.py
@@ -2,15 +2,104 @@ import pathlib
 import sys
 import os
 
+from space import Space
+from xypair import XYpair
+
+
+class Keypad(Space):
+    def __init__(self, in_put) -> None:
+        super().__init__(in_put)
+
+        # all keys are unique so simplify self.items to self.position
+        self.position: dict[str, XYpair] = {key: self.initial_position(key) for key in self.items}
+        self.gap: XYpair = self.position[' ']
+
+    def shortest_paths_to_key(self, start_key: str, finish_key: str) -> list[str]:
+        start: XYpair = self.position[start_key]
+        finish: XYpair = self.position[finish_key]
+
+        diff: XYpair = finish - start
+        if diff.x > 0:
+            horizontal_direction: str = '>'
+        elif diff.x < 0:
+            horizontal_direction = '<'
+        else:
+            horizontal_direction = ''
+
+        if diff.y > 0:
+            vertical_direction: str = 'v'
+        elif diff.y < 0:
+            vertical_direction = '^'
+        else:
+            vertical_direction = ''
+
+        horizontal_steps: str = horizontal_direction * abs(diff.x)
+        vertical_steps: str = vertical_direction * abs(diff.y)
+        paths: set[str] = set()
+        if (finish.x, start.y) != self.gap:
+            paths.add(horizontal_steps + vertical_steps)
+        if (start.x, finish.y) != self.gap:
+            paths.add(vertical_steps + horizontal_steps)
+        return list(paths)
+
+    def shortest_sequences(self, keys_to_press: str) -> list[str]:
+        paths: list[str] = ['']
+        cur_key: str = 'A'
+        for key in keys_to_press:
+            new_paths: list[str] = []
+            for path in self.shortest_paths_to_key(cur_key, key):
+                new_paths.extend(f'{p}{path}A' for p in paths)
+            paths = new_paths
+            cur_key = key
+
+        min_length: int = min(len(path) for path in paths)
+        return [path for path in paths if len(path) == min_length]
+
+
+numeric_layout: list[str] = ['789',
+                             '456',
+                             '123',
+                             ' 0A']
+numeric_keypad: Keypad = Keypad(numeric_layout)
+
+direction_layout: list[str] = [' ^A',
+                               '<v>']
+direction_keypad: Keypad = Keypad(direction_layout)
+
+
+def len_shortest_sequence(code: str) -> int:
+    # Shortest sequences for code on keypad
+    sequences: list[str] = numeric_keypad.shortest_sequences(code)
+
+    # Shortest sequences for above on direction pad
+    new_sequences: list[str] = []
+    for s in sequences:
+        new_sequences.extend(direction_keypad.shortest_sequences(s))
+    min_length: int = min(len(s) for s in new_sequences)
+    sequences = [s for s in new_sequences if len(s) == min_length]
+
+    # Shortest sequences for above on direction pad
+    new_sequences = []
+    for s in sequences:
+        new_sequences.extend(direction_keypad.shortest_sequences(s))
+    min_length = min(len(s) for s in new_sequences)
+    sequences = [s for s in new_sequences if len(s) == min_length]
+
+    return min(len(s) for s in sequences)
+
+
+def complexity(code: str) -> int:
+    return int(code.rstrip('A')) * len_shortest_sequence(code)
+
 
 def parse(puzzle_input: str):
     """Parse input"""
-    return [line for line in puzzle_input.split('\n')]
+    return puzzle_input.split('\n')
 
 
 def part1(data):
     """Solve part 1"""
-    return data
+    return sum(complexity(code) for code in data)
 
 
 def part2(data):
@@ -31,7 +120,7 @@ def solve(puzzle_input: str):
 if __name__ == '__main__':
     DIR: str = f'{os.path.dirname(sys.argv[0])}/'
 
-    PART1_TEST_ANSWER = None
+    PART1_TEST_ANSWER = 126384
     PART2_TEST_ANSWER = None
 
     file: pathlib.Path = pathlib.Path(DIR + 'part1_test.txt')
