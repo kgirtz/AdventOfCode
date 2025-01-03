09498021bb2ef9807f73c2907ea4e9465310dfbf
kgirtz <kubeman88@gmail.com>
2025-01-03 14:31:37 -0500

    Day 5 complete

--- AoC2018/Day 5/Day 5.py
+++ AoC2018/Day 5/Day 5.py
@@ -5,17 +5,38 @@ import os
 
 def parse(puzzle_input: str):
     """Parse input"""
-    return [line for line in puzzle_input.split('\n')]
+    return puzzle_input.strip()
+
+
+def opposite_polarity(a: str, b: str) -> bool:
+    return a != b and a.lower() == b.lower()
+
+
+def react(polymer: str, ignore: str = '') -> str:
+    ignore = ignore.lower()
+
+    stack: list[str] = []
+    for p in polymer:
+        if p.lower() == ignore:
+            continue
+
+        if stack and opposite_polarity(stack[-1], p):
+            stack.pop()
+        else:
+            stack.append(p)
+
+    return ''.join(stack)
 
 
 def part1(data):
     """Solve part 1"""
-    return data
+    return len(react(data))
 
 
 def part2(data):
     """Solve part 2"""
-    return data
+    units: set[str] = {u.lower() for u in data}
+    return min(len(react(data, ig)) for ig in units)
 
 
 def solve(puzzle_input: str):
@@ -31,8 +52,8 @@ def solve(puzzle_input: str):
 if __name__ == '__main__':
     DIR: str = f'{os.path.dirname(sys.argv[0])}/'
 
-    PART1_TEST_ANSWER = None
-    PART2_TEST_ANSWER = None
+    PART1_TEST_ANSWER = 10
+    PART2_TEST_ANSWER = 4
 
     file: pathlib.Path = pathlib.Path(DIR + 'part1_test.txt')
     if file.exists() and PART1_TEST_ANSWER is not None:
