8e2ccf2ad34d5d6568653a1d63036450a51c538c
kgirtz <kubeman88@gmail.com>
2025-01-12 01:41:04 -0500

    Day 8 complete

--- AoC2018/Day 8/Day 8.py
+++ AoC2018/Day 8/Day 8.py
@@ -1,21 +1,52 @@
 import pathlib
 import sys
 import os
+from collections.abc import Sequence
 
 
 def parse(puzzle_input: str):
     """Parse input"""
-    return [line for line in puzzle_input.split('\n')]
+    return [int(n) for n in puzzle_input.split()]
+
+
+class Node:
+    def __init__(self, nums: Sequence[int]) -> None:
+        num_children: int = nums[0]
+        self.children: list[Node] = []
+
+        i: int = 2
+        for _ in range(num_children):
+            self.children.append(Node(nums[i:]))
+            i += len(self.children[-1])
+
+        num_metadata: int = nums[1]
+        self.metadata: tuple[int, ...] = tuple(nums[i:i + num_metadata])
+
+    def __len__(self) -> int:
+        return 2 + len(self.metadata) + sum(len(child) for child in self.children)
+
+    def metadata_sum(self) -> int:
+        return sum(self.metadata) + sum(child.metadata_sum() for child in self.children)
+
+    def value(self) -> int:
+        if not self.children:
+            return sum(self.metadata)
+
+        total: int = 0
+        for i in self.metadata:
+            if 0 < i <= len(self.children):
+                total += self.children[i - 1].value()
+        return total
 
 
 def part1(data):
     """Solve part 1"""
-    return data
+    return Node(data).metadata_sum()
 
 
 def part2(data):
     """Solve part 2"""
-    return data
+    return Node(data).value()
 
 
 def solve(puzzle_input: str):
@@ -31,8 +62,8 @@ def solve(puzzle_input: str):
 if __name__ == '__main__':
     DIR: str = f'{os.path.dirname(sys.argv[0])}/'
 
-    PART1_TEST_ANSWER = None
-    PART2_TEST_ANSWER = None
+    PART1_TEST_ANSWER = 138
+    PART2_TEST_ANSWER = 66
 
     file: pathlib.Path = pathlib.Path(DIR + 'part1_test.txt')
     if file.exists() and PART1_TEST_ANSWER is not None:
