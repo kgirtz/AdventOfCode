46882f3dedb4861f3e29a77be926bdc0c855de9b
kgirtz <kubeman88@gmail.com>
2025-01-26 22:52:31 -0500

    Day 11: clean up

--- AoC2018/Day 11/Day 11.py
+++ AoC2018/Day 11/Day 11.py
@@ -3,38 +3,33 @@ import sys
 import os
 import functools
 
-from xypair import XYpair, XYtuple
-
 
 def parse(puzzle_input: str):
     """Parse input"""
     return int(puzzle_input)
 
 
-def power_level(pt: XYpair, grid_serial_number: int) -> int:
-    rack_id: int = pt.x + 10
-    level: int = (rack_id * pt.y + grid_serial_number) * rack_id
-    return (level % 1000) // 100 - 5
+def power_level(x: int, y: int, grid_serial_number: int) -> int:
+    rack_id: int = x + 10
+    level: int = (rack_id * y + grid_serial_number) * rack_id
+    return (level // 100) % 10 - 5
 
 
 @functools.cache
-def total_power(top_left: XYtuple, square_size: int, grid_serial_number: int) -> int:
-    if square_size <= 1:
-        top_left = XYpair(*top_left)
-        rack_id: int = top_left.x + 10
-        level: int = (rack_id * top_left.y + grid_serial_number) * rack_id
-        return (level % 1000) // 100 - 5
+def total_power(x: int, y: int, square_size: int, grid_serial_number: int) -> int:
+    if square_size == 1:
+        return power_level(x, y, grid_serial_number)
 
-    total: int = total_power(top_left, square_size - 1, grid_serial_number)
-    top_left = XYpair(*top_left)
+    total: int = total_power(x, y, square_size - 1, grid_serial_number)
 
-    bottom_row: int = top_left.y + square_size - 1
-    for x in range(top_left.x, top_left.x + square_size):
-        total += total_power(XYpair(x, bottom_row), 1, grid_serial_number)
+    # Don't repeat bottom right corner
+    bottom_row_y: int = y + square_size - 1
+    for bottom_row_x in range(x, x + square_size):
+        total += total_power(bottom_row_x, bottom_row_y, 1, grid_serial_number)
 
-    right_side: int = top_left.x + square_size - 1
-    for y in range(top_left.y, top_left.y + square_size - 1):
-        total += total_power(XYpair(right_side, y), 1, grid_serial_number)
+    right_side_x: int = x + square_size - 1
+    for right_side_y in range(y, y + square_size - 1):
+        total += total_power(right_side_x, right_side_y, 1, grid_serial_number)
 
     return total
 
@@ -42,29 +37,30 @@ def total_power(top_left: XYtuple, square_size: int, grid_serial_number: int) ->
 def part1(data):
     """Solve part 1"""
     max_power: int = 0
-    max_power_cell: XYpair = XYpair(0, 0)
-    for y in range(1, 299):
-        for x in range(1, 299):
-            power: int = total_power((x, y), 3, data)
+    max_power_cell: tuple[int, int] = (0, 0)
+    size: int = 3
+    for y in range(1, 302 - size):
+        for x in range(1, 302 - size):
+            power: int = total_power(x, y, size, data)
             if power > max_power:
                 max_power = power
-                max_power_cell = XYpair(x, y)
+                max_power_cell = (x, y)
     return max_power_cell
 
 
 def part2(data):
     """Solve part 2"""
     max_power: int = 0
-    max_power_cell: XYpair = XYpair(0, 0)
+    max_power_cell: tuple[int, int] = (0, 0)
     max_power_size: int = 0
     for size in range(1, 301):
         print(size)
         for y in range(1, 302 - size):
             for x in range(1, 302 - size):
-                power: int = total_power((x, y), size, data)
+                power: int = total_power(x, y, size, data)
                 if power > max_power:
                     max_power = power
-                    max_power_cell = XYpair(x, y)
+                    max_power_cell = (x, y)
                     max_power_size = size
     return *max_power_cell, max_power_size
 
@@ -74,7 +70,7 @@ def solve(puzzle_input: str):
     data = parse(puzzle_input)
     solution1 = part1(data)
     data = parse(puzzle_input)
-    solution2 = part2(data)
+    solution2 = None#part2(data)
 
     return solution1, solution2
 
@@ -83,7 +79,7 @@ if __name__ == '__main__':
     DIR: str = f'{os.path.dirname(sys.argv[0])}/'
 
     PART1_TEST_ANSWER = (21, 61)
-    PART2_TEST_ANSWER = (232, 251, 12)
+    PART2_TEST_ANSWER = None#(232, 251, 12)
 
     file: pathlib.Path = pathlib.Path(DIR + 'part1_test.txt')
     if file.exists() and PART1_TEST_ANSWER is not None:
