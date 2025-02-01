ce3906d5cf105965fd79a05cff4d6b19f556e624
kgirtz <kubeman88@gmail.com>
2025-02-01 17:50:40 -0500

    Day 15: complete

--- AoC2017/Day 15/Day 15.py
+++ AoC2017/Day 15/Day 15.py
@@ -1,18 +1,41 @@
+from collections.abc import Generator
 
-PART1_TEST_ANSWER = None
-PART2_TEST_ANSWER = None
+PART1_TEST_ANSWER = 588
+PART2_TEST_ANSWER = 309
 
 
 def parse(puzzle_input: str):
-    return [line for line in puzzle_input.split('\n')]
+    return [int(line.split()[-1]) for line in puzzle_input.split('\n')]
+
+
+def generator(starting_value: int, factor: int, divisor: int = 1) -> Generator[int]:
+    value: int = starting_value
+    while True:
+        if value % divisor == 0:
+            yield value
+        value *= factor
+        value %= 2147483647
+
+
+def num_matches(gen_a: Generator[int], gen_b: Generator[int], num_pairs: int) -> int:
+    return sum(next(gen_a) & 0xFFFF == next(gen_b) & 0xFFFF for _ in range(num_pairs))
 
 
 def part1(data):
-    return None
+    starting_a, starting_b = data
+    factor_a, factor_b = (16807, 48271)
+    return num_matches(generator(starting_a, factor_a),
+                       generator(starting_b, factor_b),
+                       40000000)
 
 
 def part2(data):
-    return None
+    starting_a, starting_b = data
+    factor_a, factor_b = (16807, 48271)
+    divisor_a, divisor_b = (4, 8)
+    return num_matches(generator(starting_a, factor_a, divisor_a),
+                       generator(starting_b, factor_b, divisor_b),
+                       5000000)
 
 
 # ------------- DO NOT MODIFY BELOW THIS LINE ------------- #
