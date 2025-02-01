1acac8999d8016f6d06145c4bda65c5d0bd90a88
kgirtz <kubeman88@gmail.com>
2025-02-01 16:33:46 -0500

    Day 10: complete

--- AoC2017/Day 10/Day 10.py
+++ AoC2017/Day 10/Day 10.py
@@ -1,18 +1,61 @@
+import operator
+import functools
+from collections.abc import Iterable, Sequence
 
-PART1_TEST_ANSWER = None
+PART1_TEST_ANSWER = None  # 12
 PART2_TEST_ANSWER = None
 
 
 def parse(puzzle_input: str):
-    return [line for line in puzzle_input.split('\n')]
+    return puzzle_input.strip()
+
+
+def knot_hash(lengths: Iterable[int], list_size: int, rounds: int = 1) -> list[int]:
+    cur_position: int = 0
+    skip_size: int = 0
+    lst: list[int] = list(range(list_size))
+    
+    for _ in range(rounds):
+        for length in lengths:
+            if 0 < length < list_size:
+                # Temporarily rotate list so current position is at head
+                tmp_lst: list[int] = lst[cur_position:] + lst[:cur_position]
+                
+                # Reverse length
+                tmp_lst = tmp_lst[length - 1::-1] + tmp_lst[length:]
+                
+                # Rotate list back to original position
+                split_position: int = len(lst) - cur_position
+                lst = tmp_lst[split_position:] + tmp_lst[:split_position]
+            
+            cur_position = (cur_position + length + skip_size) % len(lst)
+            skip_size += 1
+    
+    return lst
+
+
+def condense(sparse_hash: Sequence[int]) -> list[int]:
+    assert len(sparse_hash) == 256
+    
+    block_size: int = 16
+    dense_hash: list[int] = []
+    for i in range(0, len(sparse_hash), block_size):
+        dense_hash.append(functools.reduce(operator.xor, sparse_hash[i:i + block_size]))
+    return dense_hash
 
 
 def part1(data):
-    return None
+    lengths: list[int] = [int(n) for n in data.split(',')]
+    sparse_hash: Sequence[int] = knot_hash(lengths, 256)  # test = 5, input = 256
+    return sparse_hash[0] * sparse_hash[1]
 
 
 def part2(data):
-    return None
+    lengths: list[int] = [ord(ch) for ch in data] + [17, 31, 73, 47, 23]
+    sparse_hash: Sequence[int] = knot_hash(lengths, 256, rounds=64)
+    dense_hash: Iterable[int] = condense(sparse_hash)
+    hex_hash: str = ''.join(f'{n:02x}' for n in dense_hash)
+    return hex_hash
 
 
 # ------------- DO NOT MODIFY BELOW THIS LINE ------------- #
