2326c828a6bd40611c7561014df576da57c6b2f3
kgirtz <kubeman88@gmail.com>
2025-02-09 09:54:45 -0500

    aoctools: added linear and quadratic root solvers for integers

--- aoctools.py
+++ aoctools.py
@@ -1,10 +1,13 @@
-from collections.abc import Hashable, Callable
-from typing import TypeVar
+import math
+import typing
+import collections.abc
 
-_T: TypeVar = TypeVar('_T', bound=Hashable)
+_T: typing.TypeVar = typing.TypeVar('_T', bound=collections.abc.Hashable)
 
 
-def iterate_state(initial_state: _T, num_iterations: int, transformation: Callable[[_T], _T]) -> _T:
+def iterate_state(initial_state: _T, num_iterations: int, transformation: collections.abc.Callable[[_T], _T]) -> _T:
+    """ Repeatedly call transformation() on current state, cache results to detect cycles """
+
     if num_iterations < 0:
         raise ValueError('number of iterations cannot be negative')
 
@@ -22,3 +25,32 @@ def iterate_state(initial_state: _T, num_iterations: int, transformation: Callab
             return iterate_state(current_state, reduced_iterations, transformation)
 
     return current_state
+
+
+def linear_roots_int(a: int, b: int) -> int | None:
+    """ Solves ax + b = 0 for real, integer roots only """
+
+    x: int = int(-b / a)
+    if a * x + b == 0:
+        return x
+    else:
+        return None
+
+
+def quadratic_roots_int(a: int, b: int, c: int) -> tuple[int | None, ...]:
+    """ Solves ax^2 + bx + c = 0 for real, integer roots only """
+
+    discriminant: int = b ** 2 - 4 * a * c
+    if discriminant < 0:
+        return None, None
+
+    integer_roots: list[int | None] = [None, None]
+    x: int = int((-b - math.sqrt(discriminant)) / (2 * a))
+    if a * (x ** 2) + b * x + c == 0:
+        integer_roots[0] = x
+
+    x = int((-b + math.sqrt(discriminant)) / (2 * a))
+    if a * (x ** 2) + b * x + c == 0:
+        integer_roots[1] = x
+
+    return tuple(integer_roots)
