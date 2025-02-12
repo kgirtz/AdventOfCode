6377c02abd3e4f1bc356a57bce60e90d9fa13fcc
kgirtz <kubeman88@gmail.com>
2025-02-12 11:00:24 -0500

    Day 21: setup

--- AoC2017/Day 21/Day 21.py
+++ AoC2017/Day 21/Day 21.py
@@ -1,14 +1,76 @@
+from collections.abc import Iterable
 
-PART1_TEST_ANSWER = None
+from xypair import XYpair, XYtuple, ORIGIN
+
+PART1_TEST_ANSWER = 12  # 12
 PART2_TEST_ANSWER = None
 
 
+class Image:
+    def __init__(self, size: int, pixels: Iterable[XYtuple], top_left: XYtuple = ORIGIN) -> None:
+        self.position: XYpair = XYpair(*top_left)
+        self.size: int = size
+        self.active_pixels: set[XYpair] = {XYpair(*pixel) - self.position for pixel in pixels}
+
+    def pixels(self) -> set[XYpair]:
+        return {pixel + self.position for pixel in self.active_pixels}
+
+    def enhance(self) -> None:
+        if self.size % 2 == 0:
+            for y in range(0, self.size, 2):
+                for x in range(0, self.size, 2):
+                    top_left: XYpair = XYpair(x, y)
+                    sub_active: set[XYpair] = {top_left,        top_left.right(),
+                                               top_left.down(), top_left.down_right()} & self.active_pixels
+                    sub_image: Image = Image(2, sub_active, top_left)
+
+        else:
+            assert self.size % 3 == 0
+
+            for y in range(0, self.size, 3):
+                for x in range(0, self.size, 3):
+                    top_left: XYpair = XYpair(x, y)
+                    center: XYpair = top_left.down_right()
+                    sub_active: set[XYpair] = {center.up_left(),   center.up(),   center.up_right(),
+                                               center.left(),      center,        center.right(),
+                                               center.down_left(), center.down(), center.down_right()} & self.active_pixels
+                    sub_image: Image = Image(3, sub_active, top_left)
+
+
+class Rule:
+    def __init__(self, rule: str) -> None:
+        before_str, after_str = rule.strip().split(' => ')
+        before: list[str] = before_str.strip().split('/')
+        after: list[str] = after_str.strip().split('/')
+
+        self.size_before: int = len(before)
+        self.on_before: set[XYpair] = set()
+        for y in range(self.size_before):
+            for x in range(self.size_before):
+                if before[y][x] == '#':
+                    self.on_before.add(XYpair(x, y))
+
+        self.size_after: int = len(after)
+        self.on_after: set[XYpair] = set()
+        for y in range(self.size_after):
+            for x in range(self.size_after):
+                if after[y][x] == '#':
+                    self.on_after.add(XYpair(x, y))
+
+    def matches(self, image: Image) -> bool:
+        ...
+
+
 def parse(puzzle_input: str):
-    return [line for line in puzzle_input.split('\n')]
+    return [Rule(line) for line in puzzle_input.split('\n')]
 
 
 def part1(data):
-    return None
+    iterations: int = 2  # test = 2, input = 5
+    image: Image = Image(3, ((1, 0), (2, 1), (0, 2), (1, 2), (2, 2)))
+    for _ in range(iterations):
+        image.enhance()
+    return len(image.active_pixels)
 
 
 def part2(data):
