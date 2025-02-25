49ad002664ebb06d68e3c6d32168e84ec35ddc82
kgirtz <kubeman88@gmail.com>
2025-02-25 18:50:38 -0500

    Day 10: converted to new template

--- AoC2022/Day 10/Day 10.py
+++ AoC2022/Day 10/Day 10.py
@@ -1,11 +1,12 @@
-import pathlib
-import sys
-import os
 
 
-def parse(puzzle_input):
-    """Parse input"""
-    return puzzle_input.split('\n')
+PART1_TEST_ANSWER = 13140
+PART2_TEST_ANSWER = '##..##..##..##..##..##..##..##..##..##..\n' + \
+                    '###...###...###...###...###...###...###.\n' + \
+                    '####....####....####....####....####....\n' + \
+                    '#####.....#####.....#####.....#####.....\n' + \
+                    '######......######......######......####\n' + \
+                    '#######.......#######.......#######.....\n'
 
 
 def opcode(instruction: str) -> str:
@@ -80,8 +81,11 @@ def render_crt(program: list[str]) -> str:
     return ''.join(crt)
 
 
+def parse(puzzle_input: str):
+    return puzzle_input.split('\n')
+
+
 def part1(data):
-    """Solve part 1"""
     interesting_cycles: list[int] = list(range(20, 220 + 1, 40))
 
     signal_strengths: list[int] = [execute_program(data, c - 1) * c for c in interesting_cycles]
@@ -90,26 +94,92 @@ def part1(data):
 
 
 def part2(data):
-    """Solve part 2"""
-
-    return render_crt(data)
-
-
-def solve(puzzle_input):
-    """Solve the puzzle for the given input"""
-    data = parse(puzzle_input)
-    solution1 = part1(data)
-    data = parse(puzzle_input)
-    solution2 = part2(data)
-
-    return solution1, solution2
+    image: str = render_crt(data)
+    print('\n' + render_crt(data).replace('.', ' '))
+    return image
 
 
-if __name__ == "__main__":
-    DIR = f'{os.path.dirname(sys.argv[0])}/'
-    for file in ('example.txt', 'input.txt'):
-        print(f"{file}:")
-        puzzle_input = pathlib.Path(DIR + file).read_text().strip()
-        solutions = solve(puzzle_input)
-        print("\n".join(str(solution) for solution in solutions))
-        print()
+# ------------- DO NOT MODIFY BELOW THIS LINE ------------- #
+
+
+import pathlib
+
+
+def get_puzzle_input(file: pathlib.Path) -> str:
+    if not file.exists():
+        return ''
+    return file.read_text().strip('\n').replace('\t', ' ' * 4)
+
+
+def execute(func, puzzle_input: str) -> (..., int):
+    import time
+
+    start: int = time.perf_counter_ns()
+    result = func(parse(puzzle_input))
+    execution_time_us: int = (time.perf_counter_ns() - start) // 1000
+    return result, execution_time_us
+
+
+def timestamp(execution_time_us: int) -> str:
+    stamp: str = f'{round(execution_time_us / 1000000, 3)} s'
+    if execution_time_us < 1000000:
+        stamp = f'{round(execution_time_us / 1000, 3)} ms'
+    return f'\t[{stamp}]'
+
+
+def test(part_num: int, directory: str) -> None:
+    if part_num == 1:
+        func = part1
+        answer = PART1_TEST_ANSWER
+    else:
+        func = part2
+        answer = PART2_TEST_ANSWER
+
+    prefix: str = f'PART {part_num} TEST: '
+    if answer is None:
+        print(prefix + 'skipped')
+        return
+
+    file: pathlib.Path = pathlib.Path(directory, f'part{part_num}_test.txt')
+    if not file.exists():
+        file = pathlib.Path(directory, 'test.txt')
+
+    puzzle_input: str = get_puzzle_input(file)
+    if not puzzle_input:
+        print(prefix + 'no input')
+        return
+
+    result, duration = execute(func, puzzle_input)
+    result = 'PASS' if result == answer else 'FAIL'
+    print(prefix + result + timestamp(duration))
+
+
+def solve(part_num: int, directory: str) -> None:
+    func = part1 if part_num == 1 else part2
+    prefix: str = f'PART {part_num}: '
+
+    file: pathlib.Path = pathlib.Path(directory, 'input.txt')
+    if not file.exists():
+        # Download file?
+        ...
+
+    puzzle_input: str = get_puzzle_input(file)
+    if not puzzle_input:
+        print(prefix + 'no input')
+        return
+
+    result, duration = execute(func, puzzle_input)
+    suffix: str = '' if result is None else timestamp(duration)
+    print(prefix + str(result) + suffix)
+
+
+if __name__ == '__main__':
+    import os
+
+    working_directory: str = os.path.dirname(__file__)
+
+    test(1, working_directory)
+    test(2, working_directory)
+    print()
+    solve(1, working_directory)
+    solve(2, working_directory)
