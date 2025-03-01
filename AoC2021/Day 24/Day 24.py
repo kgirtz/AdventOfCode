ed2665dca02f068de55d23a131be3616fc1ee4ef
kgirtz <kubeman88@gmail.com>
2025-03-01 18:35:14 -0500

    Day 24: reimplemented with AbstractComputer

--- AoC2021/Day 24/Day 24.py
+++ AoC2021/Day 24/Day 24.py
@@ -1,43 +1,43 @@
+import typing
+from computer import AbstractComputer
 
 PART1_TEST_ANSWER = None
 PART2_TEST_ANSWER = None
 
 
-class ALU:
-    def __init__(self) -> None:
-        self.var: dict[str, int] = {reg: 0 for reg in 'wxyz'}
+class ALU(AbstractComputer):
+    def operand_value(self, op: str) -> int:
+        return self.register[op] if op.isalpha() else self.immediate_value(op)
 
-    def __str__(self) -> str:
-        return str(self.var)
-
-    def run(self, program: list[str], input_str: str = '') -> None:
-        for instruction in program:
-            input_str = self.execute(instruction, input_str)
-
-    def execute(self, instruction: str, input_str: str = '') -> str:
-        if instruction.startswith('inp'):
-            dst: str = instruction[-1]
-            self.var[dst] = int(input_str[0])
-            return input_str[1:]
-
-        mnemonic, dst, op_str = instruction.split()
-        if op_str[-1].isdigit():
-            op = int(op_str)
+    def decode(self) -> int | None:
+        self.instruction = typing.cast(str, self.instruction)
+        self.opcode, *operands = self.instruction.split()
+        if self.opcode == 'inp':
+            if not self.input_available():
+                return self.HALT
+            self.operands = operands
         else:
-            op = self.var[op_str]
+            a, b = operands
+            self.operands = (a, self.operand_value(b))
 
-        if mnemonic == 'add':
-            self.var[dst] += op
-        elif mnemonic == 'mul':
-            self.var[dst] *= op
-        elif mnemonic == 'div':
-            self.var[dst] //= op
-        elif mnemonic == 'mod':
-            self.var[dst] %= op
-        elif mnemonic == 'eql':
-            self.var[dst] = int(self.var[dst] == op)
+    def execute(self) -> None:
+        if self.opcode == 'inp':
+            a, = self.operands
+            self.register[a] = self.next_input()
+            return
 
-        return input_str
+        a, b = self.operands
+        match self.opcode:
+            case 'add':
+                self.register[a] += b
+            case 'mul':
+                self.register[a] *= b
+            case 'div':
+                self.register[a] //= b
+            case 'mod':
+                self.register[a] %= b
+            case 'eql':
+                self.register[a] = int(self.register[a] == b)
 
 
 def parse(puzzle_input: str):
@@ -45,8 +45,8 @@ def parse(puzzle_input: str):
 
 
 def part1(data):
-    monad = data
     unit: ALU = ALU()
+    unit.load_memory(data)
     for d1 in range(8, 0, -1):  # 1:8
         for d2 in range(9, 6, -1):  # 7:9
             for d3 in range(9, 0, -1):  # 1:9
@@ -61,18 +61,18 @@ def part1(data):
                                                 for d12 in range(9, 0, -1):  # 1:9
                                                     for d13 in range(3, 0, -1):  # 1:3
                                                         for d14 in range(9, 1, -1):  # 2:9
-                                                            model_num: str = ''.join(str(d) for d in [d1, d2, d3, d4,
-                                                                                                      d5, d6, d7, d8,
-                                                                                                      d9, d10, d11, d12,
-                                                                                                      d13, d14])
-                                                            unit.run(monad, model_num)
-                                                            if unit.var['z'] == 0:
-                                                                return int(model_num)
+                                                            model_num: list[int] = [d1, d2, d3, d4, d5, d6, d7, d8,
+                                                                                    d9, d10, d11, d12, d13, d14]
+                                                            unit.reset()
+                                                            unit.add_to_input_buffer(model_num)
+                                                            unit.run()
+                                                            if unit.register['z'] == 0:
+                                                                return int(''.join(str(n) for n in model_num))
 
 
 def part2(data):
-    monad = data
     unit: ALU = ALU()
+    unit.load_memory(data)
     for d1 in range(1, 9):  # 1:8
         for d2 in range(7, 10):  # 7:9
             for d3 in range(1, 10):  # 1:9
@@ -87,13 +87,13 @@ def part2(data):
                                                 for d12 in range(1, 10):  # 1:9
                                                     for d13 in range(1, 4):  # 1:3
                                                         for d14 in range(2, 10):  # 2:9
-                                                            model_num: str = ''.join(str(d) for d in [d1, d2, d3, d4,
-                                                                                                      d5, d6, d7, d8,
-                                                                                                      d9, d10, d11, d12,
-                                                                                                      d13, d14])
-                                                            unit.run(monad, model_num)
-                                                            if unit.var['z'] == 0:
-                                                                return int(model_num)
+                                                            model_num: list[int] = [d1, d2, d3, d4, d5, d6, d7, d8,
+                                                                                    d9, d10, d11, d12, d13, d14]
+                                                            unit.reset()
+                                                            unit.add_to_input_buffer(model_num)
+                                                            unit.run()
+                                                            if unit.register['z'] == 0:
+                                                                return int(''.join(str(n) for n in model_num))
 
 
 # ------------- DO NOT MODIFY BELOW THIS LINE ------------- #
