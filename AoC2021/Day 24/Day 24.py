6fa12204a3a1823afc1a6076edd1bdc619236caa
kgirtz <kubeman88@gmail.com>
2025-03-01 17:59:10 -0500

    Day 24: switched to new template

--- AoC2021/Day 24/Day 24.py
+++ AoC2021/Day 24/Day 24.py
@@ -1,6 +1,6 @@
-import pathlib
-import sys
-import os
+
+PART1_TEST_ANSWER = None
+PART2_TEST_ANSWER = None
 
 
 class ALU:
@@ -13,7 +13,6 @@ class ALU:
     def run(self, program: list[str], input_str: str = '') -> None:
         for instruction in program:
             input_str = self.execute(instruction, input_str)
-        return
 
     def execute(self, instruction: str, input_str: str = '') -> str:
         if instruction.startswith('inp'):
@@ -41,14 +40,12 @@ class ALU:
         return input_str
 
 
-def parse(puzzle_input):
-    """Parse input"""
-    return [line for line in puzzle_input.split('\n')]
+def parse(puzzle_input: str):
+    return puzzle_input.split('\n')
 
 
 def part1(data):
-    """Solve part 1"""
-    monad: list[str] = data
+    monad = data
     unit: ALU = ALU()
     for d1 in range(8, 0, -1):  # 1:8
         for d2 in range(9, 6, -1):  # 7:9
@@ -65,17 +62,16 @@ def part1(data):
                                                     for d13 in range(3, 0, -1):  # 1:3
                                                         for d14 in range(9, 1, -1):  # 2:9
                                                             model_num: str = ''.join(str(d) for d in [d1, d2, d3, d4,
-                                                                                                     d5, d6, d7, d8,
-                                                                                                     d9, d10, d11, d12,
-                                                                                                     d13, d14])
+                                                                                                      d5, d6, d7, d8,
+                                                                                                      d9, d10, d11, d12,
+                                                                                                      d13, d14])
                                                             unit.run(monad, model_num)
                                                             if unit.var['z'] == 0:
                                                                 return int(model_num)
 
 
 def part2(data):
-    """Solve part 2"""
-    monad: list[str] = data
+    monad = data
     unit: ALU = ALU()
     for d1 in range(1, 9):  # 1:8
         for d2 in range(7, 10):  # 7:9
@@ -100,21 +96,87 @@ def part2(data):
                                                                 return int(model_num)
 
 
-def solve(puzzle_input):
-    """Solve the puzzle for the given input"""
-    data = parse(puzzle_input)
-    solution1 = part1(data)
-    data = parse(puzzle_input)
-    solution2 = part2(data)
-
-    return solution1, solution2
+# ------------- DO NOT MODIFY BELOW THIS LINE ------------- #
 
 
-if __name__ == "__main__":
-    DIR = f'{os.path.dirname(sys.argv[0])}/'
-    for file in ('input.txt',):  # 'input.txt'):
-        print(f"{file}:")
-        puzzle_input = pathlib.Path(DIR + file).read_text().strip()
-        solutions = solve(puzzle_input)
-        print("\n".join(str(solution) for solution in solutions))
-        print()
+import pathlib
+
+
+def get_puzzle_input(file: pathlib.Path) -> str:
+    if not file.exists():
+        return ''
+    return file.read_text().strip('\n').replace('\t', ' ' * 4)
+
+
+def execute(func, puzzle_input: str) -> (..., int):
+    import time
+
+    start: int = time.perf_counter_ns()
+    result = func(parse(puzzle_input))
+    execution_time_us: int = (time.perf_counter_ns() - start) // 1000
+    return result, execution_time_us
+
+
+def timestamp(execution_time_us: int) -> str:
+    stamp: str = f'{round(execution_time_us / 1000000, 3)} s'
+    if execution_time_us < 1000000:
+        stamp = f'{round(execution_time_us / 1000, 3)} ms'
+    return f'\t[{stamp}]'
+
+
+def test(part_num: int, directory: str) -> None:
+    if part_num == 1:
+        func = part1
+        answer = PART1_TEST_ANSWER
+    else:
+        func = part2
+        answer = PART2_TEST_ANSWER
+
+    prefix: str = f'PART {part_num} TEST: '
+    if answer is None:
+        print(prefix + 'skipped')
+        return
+
+    file: pathlib.Path = pathlib.Path(directory, f'part{part_num}_test.txt')
+    if not file.exists():
+        file = pathlib.Path(directory, 'test.txt')
+
+    puzzle_input: str = get_puzzle_input(file)
+    if not puzzle_input:
+        print(prefix + 'no input')
+        return
+
+    result, duration = execute(func, puzzle_input)
+    result = 'PASS' if result == answer else 'FAIL'
+    print(prefix + result + timestamp(duration))
+
+
+def solve(part_num: int, directory: str) -> None:
+    func = part1 if part_num == 1 else part2
+    prefix: str = f'PART {part_num}: '
+
+    file: pathlib.Path = pathlib.Path(directory, 'input.txt')
+    if not file.exists():
+        # Download file?
+        ...
+
+    puzzle_input: str = get_puzzle_input(file)
+    if not puzzle_input:
+        print(prefix + 'no input')
+        return
+
+    result, duration = execute(func, puzzle_input)
+    suffix: str = '' if result is None else timestamp(duration)
+    print(prefix + str(result) + suffix)
+
+
+if __name__ == '__main__':
+    import os
+
+    working_directory: str = os.path.dirname(__file__)
+
+    test(1, working_directory)
+    test(2, working_directory)
+    print()
+    solve(1, working_directory)
+    solve(2, working_directory)
