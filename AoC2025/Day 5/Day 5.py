fe1ff9dd30662f9ae8cf60645055051c0e0b8d0b
kgirtz <kubeman88@gmail.com>
2025-12-05 01:20:10 -0500

    Day 5 initial

--- AoC2025/Day 5/Day 5.py
+++ AoC2025/Day 5/Day 5.py
@@ -1,18 +1,53 @@
 
-PART1_TEST_ANSWER = None
-PART2_TEST_ANSWER = None
+PART1_TEST_ANSWER = 3
+PART2_TEST_ANSWER = 14
 
 
 def parse(puzzle_input: str):
-    return [line for line in puzzle_input.split('\n')]
+    fresh_str, available_str = puzzle_input.split('\n\n')
+
+    fresh: list[tuple[int, int]] = []
+    for id_range in fresh_str.split('\n'):
+        ends: list[str] = id_range.split('-')
+        fresh.append((int(ends[0]), int(ends[1])))
+
+    available: list[int] = [int(aid) for aid in available_str.split('\n')]
+
+    return fresh, available
+
+
+def in_range(iid: int, id_range: tuple[int, int]) -> bool:
+    return id_range[0] <= iid <= id_range[1]
+
+
+def ranges_overlap(r1: tuple[int, int], r2: tuple[int, int]) -> bool:
+    return r1[1] >= r2[0] and r1[0] <= r2[1]
 
 
 def part1(data):
-    return None
+    fresh, available = data
+    return sum(any(in_range(iid, id_range) for id_range in fresh) for iid in available)
 
 
 def part2(data):
-    return None
+    fresh: set[tuple[int, int]] = set(data[0])
+
+    # Merge overlapping ranges
+    keep_merging: bool = True
+    while keep_merging:
+        keep_merging = False
+        temp_merged: set[tuple[int, int]] = set()
+        while fresh:
+            a: tuple[int, int] = fresh.pop()
+            for b in list(fresh):
+                if ranges_overlap(a, b):
+                    keep_merging = True
+                    a = (min(a[0], b[0]), max(a[1], b[1]))
+                    fresh.remove(b)
+            temp_merged.add(a)
+        fresh = temp_merged
+
+    return sum((id_range[1] - id_range[0] + 1) for id_range in fresh)
 
 
 # ------------- DO NOT MODIFY BELOW THIS LINE ------------- #
