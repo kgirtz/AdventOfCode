c0eae5380d160335eb31cbcf4e4379feae06077e
kgirtz <kubeman88@gmail.com>
2025-12-10 13:06:03 -0500

    Day 10 part 1

--- AoC2025/Day 10/Day 10.py
+++ AoC2025/Day 10/Day 10.py
@@ -1,18 +1,92 @@
+from collections.abc import Sequence
 
-PART1_TEST_ANSWER = None
-PART2_TEST_ANSWER = None
+PART1_TEST_ANSWER = 7
+PART2_TEST_ANSWER = 33
+
+
+class Machine:
+    def __init__(self, lights: str, buttons: str, joltages: str) -> None:
+        self.lights: list[bool] = [light == '#' for light in lights]
+        self.buttons: list[tuple[int, ...]] = []
+        self.joltages: list[int] = [int(j) for j in joltages.split(',')]
+
+        for button in buttons.strip().split():
+            button = button.lstrip('(').rstrip(')')
+            self.buttons.append(tuple(int(b) for b in button.split(',')))
+
+    def fewest_button_presses_lights(self) -> int:
+        fewest: dict[tuple[bool, ...], int] = {}
+        edge: set[tuple[bool, ...]] = {tuple(False for _ in self.lights)}
+        distance: int = 0
+        while edge:
+            new_edge: set[tuple[bool, ...]] = set()
+            for state in edge:
+                fewest[state] = distance
+                for button in self.buttons:
+                    next_state: tuple[bool, ...] = update_lights(state, button)
+                    if next_state not in fewest and next_state not in edge:
+                        new_edge.add(next_state)
+
+            edge = new_edge
+            distance += 1
+
+        return fewest[tuple(self.lights)]
+
+    def fewest_button_presses_joltages(self) -> int:
+        fewest: dict[tuple[int, ...], int] = {}
+        edge: set[tuple[int, ...]] = {tuple(0 for _ in self.joltages)}
+        distance: int = 0
+        while edge:
+            new_edge: set[tuple[int, ...]] = set()
+            for state in edge:
+                fewest[state] = distance
+                for button in self.buttons:
+                    next_state: tuple[int, ...] = update_joltages(state, button)
+                    if next_state not in fewest and next_state not in edge:
+                        if all(n <= j for n, j in zip(next_state, self.joltages)):
+                            new_edge.add(next_state)
+
+            edge = new_edge
+            distance += 1
+
+        return fewest[tuple(self.joltages)]
+
+
+def update_lights(lights: Sequence[bool], button_pressed: tuple[int, ...]) -> tuple[bool, ...]:
+    lights = list(lights)
+    for i in button_pressed:
+        lights[i] ^= True
+    return tuple(lights)
+
+
+def update_joltages(joltages: Sequence[int], button_pressed: tuple[int, ...]) -> tuple[int, ...]:
+    joltages = list(joltages)
+    for i in button_pressed:
+        joltages[i] += 1
+    return tuple(joltages)
 
 
 def parse(puzzle_input: str):
-    return [line for line in puzzle_input.split('\n')]
+    machines: list[Machine] = []
+    for line in puzzle_input.split('\n'):
+        light_str, line = line.split(']')
+        light_str = light_str.strip().lstrip('[')
+
+        button_str, joltage_str = line.strip().split('{')
+        button_str = button_str.strip()
+        joltage_str = joltage_str.strip().rstrip('}')
+
+        machines.append(Machine(light_str, button_str, joltage_str))
+
+    return machines
 
 
 def part1(data):
-    return None
+    return sum(machine.fewest_button_presses_lights() for machine in data)
 
 
 def part2(data):
-    return None
+    return sum(machine.fewest_button_presses_joltages() for machine in data)
 
 
 # ------------- DO NOT MODIFY BELOW THIS LINE ------------- #
