6daaee86cf504d3a56622d606fc2a1ac86d9d101
kgirtz <kubeman88@gmail.com>
2025-12-16 16:21:45 -0500

    Day 9 clean up

--- AoC2025/Day 9/Day 9.py
+++ AoC2025/Day 9/Day 9.py
@@ -1,15 +1,11 @@
 import itertools
 import heapq
-import typing
-from collections.abc import Sequence, Iterable
+from collections.abc import Sequence
 
 from xypair import XYpair
 
 PART1_TEST_ANSWER = 50
 PART2_TEST_ANSWER = 24
-
-
-XYPairHeap: typing.TypeAlias = list[tuple[int, XYpair, XYpair]]
 
 
 def parse(puzzle_input: str):
@@ -20,69 +16,53 @@ def parse(puzzle_input: str):
     return tiles
 
 
-def size_of_rectangle(tile1: XYpair, tile2: XYpair) -> int:
-    return (tile1.x_distance(tile2) + 1) * (tile1.y_distance(tile2) + 1)
-
-
-def build_rectangle_size_heap(tiles: Iterable[XYpair]) -> XYPairHeap:
-    rectangle_heap: XYPairHeap = []
-    for t1, t2 in itertools.combinations(tiles, 2):
-        heapq.heappush(rectangle_heap, (-size_of_rectangle(t1, t2), t1, t2))
-    return rectangle_heap
-
-
-def tiles_between_pairs(corner_tiles: Sequence[XYpair]) -> set[XYpair]:
-    tile_borders: set[XYpair] = set()
-    for t1, t2 in itertools.pairwise(list(corner_tiles) + [corner_tiles[0]]):
-        if t1.x == t2.x:
-            tile_borders.update(XYpair(t1.x, y) for y in range(min(t1.y, t2.y), max(t1.y, t2.y) + 1))
-        else:
-            tile_borders.update(XYpair(x, t1.y) for x in range(min(t1.x, t2.x), max(t1.x, t2.x) + 1))
-    return tile_borders
+def size_of_rectangle(corner1: XYpair, corner2: XYpair) -> int:
+    return (corner1.x_distance(corner2) + 1) * (corner1.y_distance(corner2) + 1)
 
 
 class Floor:
     def __init__(self, red_tiles: Sequence[XYpair]) -> None:
-        self.red_tiles: list[XYpair] = list(red_tiles)# + [red_tiles[0]]  # For cyclical pairwise iteration
+        self.red_tiles: list[XYpair] = list(red_tiles)
 
-        self.x_min: int = min(t.x for t in self.red_tiles)
-        self.x_max: int = max(t.x for t in self.red_tiles)
-        self.y_min: int = min(t.y for t in self.red_tiles)
-        self.y_max: int = max(t.y for t in self.red_tiles)
+        # Organize corner tile pairs for quick access
+        self.vertical_segments: list[tuple[XYpair, XYpair, bool]] = []
+        self.horizontal_segments: list[tuple[XYpair, XYpair, bool]] = []
+        for i, t1 in enumerate(self.red_tiles):
+            t0: XYpair = self.red_tiles[(i - 1) % len(self.red_tiles)]
+            t2: XYpair = self.red_tiles[(i + 1) % len(self.red_tiles)]
+            t3: XYpair = self.red_tiles[(i + 2) % len(self.red_tiles)]
 
-        # self.border_tiles: set[XYpair] = tiles_between_pairs(self.red_tiles)
+            # Vertical
+            if t1.x == t2.x:
+                is_s_curve: bool = (t1.x - t0.x) * (t1.x - t3.x) < 0
+                if t1.y > t2.y:
+                    t1, t2 = t2, t1
+                self.vertical_segments.append((t1, t2, is_s_curve))
 
-    def contains(self, tile: XYpair) -> bool:
-        return self.x_min <= tile.x <= self.x_max and self.y_min <= tile.y <= self.y_max
+            # Horizontal
+            else:
+                is_s_curve = (t1.y - t0.y) * (t1.y - t3.y) < 0
+                if t1.x > t2.x:
+                    t1, t2 = t2, t1
+                self.horizontal_segments.append((t1, t2, is_s_curve))
 
-    # def tiles_in_column(self, x: int) -> list[tuple[int, int]]:
-    #     tile_intervals: list[tuple[int, int]] = []
-    #
-    #     start: int = -1
-    #     for y in range(self.y_min, self.y_max + 2):
-    #         if start == -1 and (x, y) in self.border_tiles:
-    #             start = y
-    #         elif start != -1 and (x, y) not in self.border_tiles:
-    #             tile_intervals.append((start, y - 1))
-    #             start = -1
-    #
-    #     return tile_intervals
+        self.vertical_segments.sort()
+        self.horizontal_segments.sort()
 
     def tile_intervals_in_column(self, x: int) -> list[tuple[int, int]]:
         intersectors: list[tuple[int, int, bool]] = []
-        for i, t1 in enumerate(self.red_tiles):
-            t2: XYpair = self.red_tiles[(i + 1) % len(self.red_tiles)]
 
-            # Collinear
-            if t1.x == t2.x == x:
-                t0: XYpair = self.red_tiles[(i - 1) % len(self.red_tiles)]
-                t3: XYpair = self.red_tiles[(i + 2) % len(self.red_tiles)]
-                is_s_curve: bool = (t1.x - t0.x) * (t1.x - t3.x) < 0
-                intersectors.append((min(t1.y, t2.y), max(t1.y, t2.y), is_s_curve))
+        for t1, t2, is_s_curve in self.vertical_segments:
+            if t1.x == x:
+                intersectors.append((t1.y, t2.y, is_s_curve))
+            elif t1.x > x:
+                break
 
-            # Perpendicular, endpoints are covered by collinear case
-            elif min(t1.x, t2.x) < x < max(t1.x, t2.x):
-                intersectors.append((min(t1.y, t2.y), max(t1.y, t2.y), False))
+        for t1, t2, is_s_curve in self.horizontal_segments:
+            if t1.x < x < t2.x:
+                intersectors.append((t1.y, t1.y, is_s_curve))
+            elif t1.x > x:
+                break
 
         intersectors.sort()
 
@@ -107,19 +87,14 @@ class Floor:
 
     def tile_intervals_in_row(self, y: int) -> list[tuple[int, int]]:
         intersectors: list[tuple[int, int, bool]] = []
-        for i, t1 in enumerate(self.red_tiles):
-            t2: XYpair = self.red_tiles[(i + 1) % len(self.red_tiles)]
 
-            # Collinear
-            if t1.y == t2.y == y:
-                t0: XYpair = self.red_tiles[(i - 1) % len(self.red_tiles)]
-                t3: XYpair = self.red_tiles[(i + 2) % len(self.red_tiles)]
-                is_s_curve: bool = (t1.y - t0.y) * (t1.y - t3.y) < 0
-                intersectors.append((min(t1.x, t2.x), max(t1.x, t2.x), is_s_curve))
+        for t1, t2, is_s_curve in self.horizontal_segments:
+            if t1.y == y:
+                intersectors.append((t1.x, t2.x, is_s_curve))
 
-            # Perpendicular, endpoints are covered by collinear case
-            elif min(t1.y, t2.y) < y < max(t1.y, t2.y):
-                intersectors.append((min(t1.x, t2.x), max(t1.x, t2.x), False))
+        for t1, t2, is_s_curve in self.vertical_segments:
+            if t1.y < y < t2.y:
+                intersectors.append((t1.x, t1.x, is_s_curve))
 
         intersectors.sort()
 
@@ -142,162 +117,52 @@ class Floor:
 
         return tile_intervals
 
-    # def tiles_in_row(self, y: int) -> list[tuple[int, int]]:
-    #     tile_intervals: list[tuple[int, int]] = []
-    #
-    #     start: int = -1
-    #     for x in range(self.x_min, self.x_max + 2):
-    #         if start == -1 and (x, y) in self.border_tiles:
-    #             start = x
-    #         elif start != -1 and (x, y) not in self.border_tiles:
-    #             tile_intervals.append((start, x - 1))
-    #             start = -1
-    #
-    #     return tile_intervals
-
-    # def first_tile_above(self, pt: XYpair) -> XYpair | None:
-    #     tiles_above: set[XYpair] = {t for t in self.border_tiles if t.x == pt.x and t.y < pt.y}
-    #     if not tiles_above:
-    #         return None
-    #     return max(tiles_above, key=lambda t: t.y)
-    #
-    # def first_tile_below(self, pt: XYpair) -> XYpair | None:
-    #     tiles_below: set[XYpair] = {t for t in self.border_tiles if t.x == pt.x and t.y > pt.y}
-    #     if not tiles_below:
-    #         return None
-    #     return min(tiles_below, key=lambda t: t.y)
-    #
-    # def first_tile_to_left(self, pt: XYpair) -> XYpair | None:
-    #     tiles_left: set[XYpair] = {t for t in self.border_tiles if t.y == pt.y and t.x < pt.x}
-    #     if not tiles_left:
-    #         return None
-    #     return max(tiles_left)
-    #
-    # def first_tile_to_right(self, pt: XYpair) -> XYpair | None:
-    #     tiles_right: set[XYpair] = {t for t in self.border_tiles if t.y == pt.y and t.x > pt.x}
-    #     if not tiles_right:
-    #         return None
-    #     return min(tiles_right)
-
 
 def part1(data):
     return max(size_of_rectangle(t1, t2) for t1, t2 in itertools.combinations(data, 2))
 
 
-# 4634026886 is too high
 def part2(data):
     floor: Floor = Floor(data)
 
-    # Sort all possible rectangles by size
-    rectangle_size_heap: XYPairHeap = build_rectangle_size_heap(data)
+    # Heap sort rectangles by size
+    rectangles: list[tuple[int, XYpair, XYpair]] = []
+    for t1, t2 in itertools.combinations(data, 2):
+        heapq.heappush(rectangles, (-size_of_rectangle(t1, t2), t1, t2))
 
-    # max_size: int = 0
-    while rectangle_size_heap:
-    # for t1, t2 in itertools.combinations(data, 2):
-        rectangle_size, t1, t2 = heapq.heappop(rectangle_size_heap)
-        rectangle_size = -rectangle_size
-        print(f'Checking rectangle at {t1}, {t2} (size = {rectangle_size})')
+    while rectangles:
+        rectangle_size, t1, t2 = heapq.heappop(rectangles)
 
-        sides: list[tuple[XYpair, XYpair]] = [(t1, XYpair(t2.x, t1.y)),
-                                              (XYpair(t2.x, t1.y), t2),
-                                              (t2, XYpair(t1.x, t2.y)),
-                                              (XYpair(t1.x, t2.y), t1)]
+        t3: XYpair = XYpair(t2.x, t1.y)
+        t4: XYpair = XYpair(t1.x, t2.y)
+        sides: list[tuple[XYpair, XYpair]] = [(t1, t3),
+                                              (t3, t2),
+                                              (t2, t4),
+                                              (t4, t1)]
 
-        valid_sides: list[bool] = []
+        valid_rectangle: bool = True
         for a, b in sides:
+            valid_side: bool = False
+
             if a.x == b.x:
                 intervals: list[tuple[int, int]] = floor.tile_intervals_in_column(a.x)
-                for i1, i2 in intervals:
-                    if i1 <= min(a.y, b.y) and max(a.y, b.y) <= i2:
-                        valid_sides.append(True)
+                for y1, y2 in intervals:
+                    if y1 <= min(a.y, b.y) and max(a.y, b.y) <= y2:
+                        valid_side = True
                         break
             else:
                 intervals = floor.tile_intervals_in_row(a.y)
-                for i1, i2 in intervals:
-                    if i1 <= min(a.x, b.x) and max(a.x, b.x) <= i2:
-                        valid_sides.append(True)
+                for x1, x2 in intervals:
+                    if x1 <= min(a.x, b.x) and max(a.x, b.x) <= x2:
+                        valid_side = True
                         break
 
-        if len(valid_sides) == 4:
-            return rectangle_size
-            # max_size = max(max_size, size_of_rectangle(t1, t2))
+            if not valid_side:
+                valid_rectangle = False
+                break
 
-    # return max_size
-
-        # # Check if all red & green tiles in the rectangle
-        # corners: list[XYpair] = [t1, XYpair(t2.x, t1.y), t2, XYpair(t1.x, t2.y)]
-        # tiles_to_check: set[XYpair] = tiles_between_pairs(corners) - floor.border_tiles
-        # if not tiles_to_check:
-        #     return rectangle_size
-        #
-        # # Explore out in straight lines to check other tiles
-        # top_lim = 1_000_000_000_000
-        # bottom_lim = 0
-        # left_lim = 1_000_000_000_000
-        # right_lim = 0
-        # for t in tiles_to_check:
-        #     top_lim = min(top_lim, t.y)
-        #     bottom_lim = max(bottom_lim, t.y)
-        #     left_lim = min(left_lim, t.x)
-        #     right_lim = max(right_lim, t.x)
-        #
-        # top_tiles = set()
-        # bottom_tiles = set()
-        # left_tiles = set()
-        # right_tiles = set()
-        # for t in tiles_to_check:
-        #     if t.y == top_lim:
-        #         top_tiles.add(t)
-        #     if t.y == bottom_lim:
-        #         bottom_tiles.add(t)
-        #     if t.x == left_lim:
-        #         left_tiles.add(t)
-        #     if t.x == right_lim:
-        #         right_tiles.add(t)
-        #
-        # top_left = min(top_tiles)
-        # top_right = max(top_tiles)
-        # if floor.first_tile_above(top_left) is None or floor.first_tile_to_left(top_left) is None or \
-        #    floor.first_tile_above(top_right) is None or floor.first_tile_to_right(top_right) is None:
-        #     continue
-        #
-        # bottom_left = min(bottom_tiles)
-        # bottom_right = max(bottom_tiles)
-        # if floor.first_tile_below(bottom_left) is None or floor.first_tile_to_left(bottom_left) is None or \
-        #    floor.first_tile_below(bottom_right) is None or floor.first_tile_to_right(bottom_right) is None:
-        #     continue
-        #
-        # left_top = min(left_tiles, key=lambda t: t.y)
-        # left_bottom = max(left_tiles, key=lambda t: t.y)
-        # if floor.first_tile_above(left_top) is None or floor.first_tile_to_left(left_top) is None or \
-        #    floor.first_tile_below(left_bottom) is None or floor.first_tile_to_left(left_bottom) is None:
-        #     continue
-        #
-        # right_top = min(right_tiles, key=lambda t: t.y)
-        # right_bottom = max(right_tiles, key=lambda t: t.y)
-        # if floor.first_tile_above(right_top) is None or floor.first_tile_to_right(right_top) is None or \
-        #    floor.first_tile_below(right_bottom) is None or floor.first_tile_to_right(right_bottom) is None:
-        #     continue
-        #
-        # print('Trying flood method...')
-        #
-        # # Flood fill to completely check other tiles
-        # flood: set[XYpair] = set()
-        # border: set[XYpair] = set(tiles_to_check)
-        # valid: bool = True
-        # while border and valid:
-        #     tile: XYpair = border.pop()
-        #     flood.add(tile)
-        #     for n in tile.neighbors():
-        #         if n in floor.border_tiles or n in flood:
-        #             continue
-        #         if not floor.contains(n):
-        #             valid = False
-        #             break
-        #         border.add(n)
-        #
-        # if valid:
-        #     return rectangle_size
+        if valid_rectangle:
+            return -rectangle_size
 
 
 # ------------- DO NOT MODIFY BELOW THIS LINE ------------- #
